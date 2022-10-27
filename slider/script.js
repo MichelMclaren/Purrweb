47cef2562420df1fd884c8c67c65de70cf5d5949
Micha <neopoznono@mail.ru>
2022-10-27 19:32:32 +0600

    Finished Work

--- slider/script.js
+++ slider/script.js
@@ -1,13 +1,8 @@
-const slideContainer = document.querySelector(".container");
 const slide = document.getElementById("window");
 const nextBtn = document.getElementById("next-btn");
 const prevBtn = document.getElementById("prev-btn");
-const interval = 3000;
-
 let slides = document.querySelectorAll(".slide");
 let index = 1;
-let clicks = 1;
-let slideId;
 let currentDot;
 
 const firstClone = slides[0].cloneNode(true);
@@ -22,14 +17,6 @@ slide.append(firstClone);
 const slideWidth = slides[index].clientWidth;
 
 slide.style.transform = `translateX(${-slideWidth * index}px)`;
-
-// const startSlide = () => {
-//    slideId = setInterval(() => {
-//       moveToNextSlide();
-//       setActiveDot();
-//    }, interval);
-// };
-
 const turnOffBtns = () => {
    prevBtn.style.pointerEvents = "none";
    nextBtn.style.pointerEvents = "none";
@@ -43,16 +30,13 @@ const turnOnBtns = () => {
 const getSlides = () => document.querySelectorAll(".slide");
 
 const flipSlide = () => {
-   console.log("ИНДЕКС!! "+index)
    slide.style.transition = "0.7s ease-out";
    slide.style.transform = `translateX(${-slideWidth * index}px)`;
    turnOffBtns();
 };
 
 const setActiveDot = () => {
-   console.log(" Индекс - "+index+" "+"Количество слайдов "+(slides.length-1))
    if (index != (slides.length - 1) && index!=0) {
-      console.log("Пизда");
       currentDot = document.getElementById(`dot-${index}`);
       currentDot.classList.add("active");
    } else {
@@ -67,7 +51,6 @@ const setActiveDot = () => {
 };
 const disableDot = () => {
       currentDot = document.getElementsByClassName('active');
-      // console.log(currentDot[0].className());
       currentDot[0].classList.remove("active");
 };
 
@@ -88,7 +71,6 @@ slide.addEventListener("transitionend", () => {
 });
 
 const moveToNextSlide = () => {
-   console.log("***"+index);
    index++;
    disableDot();
    setActiveDot();
@@ -97,7 +79,6 @@ const moveToNextSlide = () => {
 };
 
 const moveToPreviousSlide = () => {
-   console.log("Стало "+index);
    index--;
    flipSlide();
    disableDot();
@@ -111,13 +92,11 @@ const checkOutsideClick = ({target}) => {
          (e) => e === target
       )+1;
       // alert("Вы нажали на "+index)
-      console.log("****"+index)
       disableDot();
       flipSlide();
       setActiveDot();
    }
 };
-console.log(index);
 document.body.addEventListener("click", checkOutsideClick);
 
 nextBtn.addEventListener("click", moveToNextSlide);
